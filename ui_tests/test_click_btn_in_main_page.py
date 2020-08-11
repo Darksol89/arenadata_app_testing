19091e34e1c3d9b5ee0979642897ff4b60583ca8
Ivan_Chistov <Darksol89@gmail.com>
2020-08-11 13:42:15 +0300

    Updated UI test. Added new test case

--- ui_tests/test_click_btn_in_main_page.py
+++ ui_tests/test_click_btn_in_main_page.py
@@ -1,16 +1,35 @@
 import pytest
 import allure
 from resources.pages.MainPage import MainPage
+from resources.locators.Locators import MainLocators
+
+url = 'http://localhost:5000'
 
 
-
-@allure.title('Click button in Main Page site')
-@pytest.mark.parametrize('template_file', ['yandex_template.yml'])
-def test_click_button_in_main_page(api_client, get_template_directory,
-                                   template_file):
+@allure.title('Check and click button in Main Page site')
+@pytest.mark.parametrize('template_file', ['google_template.yml'])
+def test_available_button_in_main_page(browser_driver, api_client,
+                                       get_template_directory, template_file):
     api_client.upload_template(template_file=template_file)
+    # Getting template name without format
     template_name = template_file.split(sep='.')
     api_client.install_template(template_name[0])
-    browser = browser_driver()
-    open_url = get_url
-    assert MainPage(browser).click_test_button()
+    # Opening url
+    browser_driver.get(url)
+
+    assert MainPage(browser_driver).click_test_button()
+
+
+@allure.title('Check of unavailable button in Main Page site')
+@pytest.mark.parametrize('template_file', ['yandex_template_no_link.yml'])
+def test_unavailable_button_in_main_page(browser_driver, api_client,
+                                         get_template_directory, template_file):
+    api_client.upload_template(template_file=template_file)
+    # Getting template name without format
+    template_name = template_file.split(sep='.')
+    api_client.install_template(template_name[0])
+    # Opening url
+    browser_driver.get(url)
+
+    assert browser_driver.find_element(*MainLocators.BTN_NO_LINK)
+    assert not browser_driver.find_element(*MainLocators.BTN_NO_LINK).click()
