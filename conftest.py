4d5cca7b61bb5c5541784793a4c514f3ae9e63d8
Ivan_Chistov <Darksol89@gmail.com>
2020-08-11 13:46:50 +0300

    Added fixture for ui testing. Added docstrings

--- conftest.py
+++ conftest.py
@@ -1,11 +1,13 @@
 import pytest
-import requests
+import allure
 import os
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from utils.api import Api
 
 
 def pytest_addoption(parser):
-    """Parser for command0line parameters"""
+    """Parser parameters for testing"""
     parser.addoption('--url',
                      action='store',
                      default='http://localhost:5000',
@@ -18,17 +20,29 @@ def pytest_addoption(parser):
 
 @pytest.fixture()
 def api_client(request):
-    """Get url for tests"""
+    """Getting url for api tests"""
     base_url = request.config.getoption('--url')
     return Api(base_url)
 
 
 @pytest.fixture()
+@allure.step('Getting working directory with templates')
 def get_template_directory(request):
-    """Get working directory with templates"""
+    """Getting working directory with templates"""
     template_directory = request.config.getoption('--template_dir')
     os.chdir(template_directory)
     return template_directory
 
 
+@pytest.fixture()
+def browser_driver():
+    """Initializing webdriver and start web browser"""
+    print('Start Chrome browser...\n')
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('--ignore-certificate-errors')
+    browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
+    yield browser
+    print('Close Chrome browser...\n')
+    browser.quit()
