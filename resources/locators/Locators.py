2c9ffdad0792ffa78254bdb7266dc3b04a74f5ad
Ivan_Chistov <Darksol89@gmail.com>
2020-08-11 13:35:06 +0300

    Button locators were updated

--- resources/locators/Locators.py
+++ resources/locators/Locators.py
@@ -1,5 +1,7 @@
+"""Locators for web elements"""
 from selenium.webdriver.common.by import By
 
 
 class MainLocators:
-    JUST_BUTTON = (By.CSS_SELECTOR, 'a[role="button"]')
+    BTN_LINK = (By.CSS_SELECTOR, 'a[role="button"]')
+    BTN_NO_LINK = (By.CSS_SELECTOR, 'button.disabled')
